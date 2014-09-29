cab2faf71d7b8d0e0c8950b2315879b7f39e835f
Marcin Rataj <lidel@lidel.org>
2014-09-29 23:49:33 +0200

    Add jshint to a test suite

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,14 +2,21 @@ module.exports = function(grunt) {
   grunt.initConfig({
     qunit: {
       files: ['test/index.html']
-    }
+    },
+    jshint: {
+      all: ['Gruntfile.js', 'yt-looper.js', 'test/**/*.js'],
+      options: {
+        '-W014': true, // ignore [W014] Bad line breaking
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
 
   // Default task.
   grunt.registerTask('test', 'qunit');
   // Travis CI task.
-  grunt.registerTask('travis', 'test');
+  grunt.registerTask('travis', ['test','jshint']);
 };
 // vim:ts=2:sw=2:et:
