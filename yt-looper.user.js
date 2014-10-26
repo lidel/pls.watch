2a15e5296bd411c397ddeaac37fe1d5fcff32ef3
Marcin Rataj <lidel@lidel.org>
2014-10-26 18:34:33 +0100

    Userscript: simplify URL for simple loop, closes #49

--- yt-looper.user.js
+++ yt-looper.user.js
@@ -7,7 +7,7 @@
 // @include     http://youtube.com/watch*
 // @include     https://www.youtube.com/watch*
 // @include     https://youtube.com/watch*
-// @version     1.2.3
+// @version     1.2.4
 // @updateURL   https://raw.github.com/lidel/yt-looper/master/yt-looper.user.js
 // @downloadURL https://raw.github.com/lidel/yt-looper/master/yt-looper.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js
@@ -31,14 +31,20 @@ var ytLooperLoaded = false;
     return sec === 0 ? '0s' : hours+minutes+seconds;
   };
 
+  var getYtPlayer = function() {
+    var doc = window.document;
+    var ytplayer = doc.getElementById('movie_player') || doc.getElementById('movie_player-flash');
+
+    return ytplayer;
+  };
+
   window.onYouTubePlayerReady = function() {
 
     if (!ytLooperLoaded) {
       var $secondaryActions = $('div.watch-secondary-actions');
       var $button = $('<button id="yt-looper" class="yt-uix-button yt-uix-button-opacity yt-uix-button-has-icon"><span style="margin:0 0.1em;font-size:2.25em;vertical-align: middle;">&#x21BB;</span> yt.aergia.eu </button>');
 
-      var doc = window.document;
-      var ytplayer = doc.getElementById('movie_player') || doc.getElementById('movie_player-flash');
+      var ytplayer = getYtPlayer();
       var start = humanize(0);
       var end = humanize(ytplayer.getDuration());
 
@@ -57,7 +63,10 @@ var ytLooperLoaded = false;
         url = url.replace(/.*youtube.com\/watch/, 'https://yt.aergia.eu/');
         url = url.replace(/&?feature=[^&#]*/g, '');
         url = url.replace(/[&#]?t=[^&#]*/g, '');
-        window.open(url + ':t=' + $('#yt-looper-start').val() + ';' + $('#yt-looper-end').val());
+        if (getYtPlayer().getPlayerState() === 2) {
+          url = url + '#t=' + $('#yt-looper-start').val() + ';' + $('#yt-looper-end').val();
+        }
+        window.open(url);
       });
 
       window.ytPlayerStateChanged = function (state) {
