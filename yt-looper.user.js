2a9391f906634fec2311b2f2b3a68e919293e3bc
Marcin Rataj <lidel@lidel.org>
2015-03-30 23:08:25 +0200

    Fixed userscript to work on playlist pages

--- yt-looper.user.js
+++ yt-looper.user.js
@@ -1,20 +1,18 @@
 // ==UserScript==
 // @name        yt-looper
 // @description Adds a button on YouTube to open current video in yt.aergia.eu looper
-// @namespace   http://yt.aergia.eu
-// @icon        http://i.imgur.com/EGgL1nx.png
+// @namespace   https://yt.aergia.eu
+// @icon        https://i.imgur.com/EGgL1nx.png
 // @include     http://www.youtube.com/watch*
 // @include     http://youtube.com/watch*
 // @include     https://www.youtube.com/watch*
 // @include     https://youtube.com/watch*
-// @version     1.3.1
+// @version     1.4.1
 // @updateURL   https://yt.aergia.eu/yt-looper.user.js
 // @downloadURL https://yt.aergia.eu/yt-looper.user.js
 // @require     https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js
 // @grant       none
 // ==/UserScript==
-
-var ytLooperLoaded = false;
 
 (function (undefined) {
 
@@ -38,52 +36,63 @@ var ytLooperLoaded = false;
     return ytplayer;
   };
 
-  window.onYouTubePlayerReady = function() {
+  var renderLooperActions = function() {
+    $('#yt-looper-start').remove();
+    $('#yt-looper-end').remove();
+    $('#yt-looper').remove();
+    $('#yt-looper-interval').remove();
+    var $button = $('<button id="yt-looper" class="yt-uix-button yt-uix-button-opacity yt-uix-button-has-icon"><span style="margin:0 0.1em;font-size:2.25em;vertical-align: middle;">&#x21BB;</span> yt.aergia.eu </button>').hide();
+    var $intervals = $('<span id="yt-looper-interval"></span>').hide();
+    var input = '<input class="yt-uix-form-input-text title-input" style="width:50px!important;margin-right:0.25em;" />';
 
-    if (!ytLooperLoaded) {
-      var $secondaryActions = $('div.watch-secondary-actions');
-      var $button = $('<button id="yt-looper" class="yt-uix-button yt-uix-button-opacity yt-uix-button-has-icon"><span style="margin:0 0.1em;font-size:2.25em;vertical-align: middle;">&#x21BB;</span> yt.aergia.eu </button>');
+    var $secondaryActions = $('div.watch-action-buttons > div.watch-secondary-actions');
+    var start = humanize(0);
+    var end = humanize(getYtPlayer().getDuration());
 
-      var ytplayer = getYtPlayer();
-      var start = humanize(0);
-      var end = humanize(ytplayer.getDuration());
+    var $start = $(input).attr('id','yt-looper-start').val(start).attr('title','Start of loop');
+    var $end   = $(input).attr('id','yt-looper-end').val(end).attr('title','End of loop');
 
-      var $intervals = $('<span id="yt-looper-interval"></span>').hide();
-      var input = '<input class="yt-uix-form-input-text title-input" style="width:50px!important;margin-right:0.25em;" />';
-      var $start = $(input).attr('id','yt-looper-start').val(start).attr('title','Start of loop');
-      var $end   = $(input).attr('id','yt-looper-end').val(end).attr('title','End of loop');
+    $secondaryActions.prepend($button);
+    $intervals.append($start).append($end).insertBefore($button);
+    $button.fadeIn();
 
-      $secondaryActions.prepend($button);
-      $intervals.append($start).append($end).insertBefore($button);
+    $button.click(function () {
+      var url = window.location.href;
+      url = url.replace(/.*youtube.com\/watch/, 'https://yt.aergia.eu/');
+      url = url.replace(/&?feature=[^&#]*/g, '');
+      url = url.replace(/[&#]t=[^&#]*/g, '');
+      if (getYtPlayer().getPlayerState() === 2) {
+        url = url + '#t=' + $('#yt-looper-start').val() + ';' + $('#yt-looper-end').val();
+      }
+      window.open(url);
+    });
 
-      ytLooperLoaded = true;
+  };
 
-      $button.click(function () {
-        var url = window.location.href;
-        url = url.replace(/.*youtube.com\/watch/, 'https://yt.aergia.eu/');
-        url = url.replace(/&?feature=[^&#]*/g, '');
-        url = url.replace(/[&#]t=[^&#]*/g, '');
-        if (getYtPlayer().getPlayerState() === 2) {
-          url = url + '#t=' + $('#yt-looper-start').val() + ';' + $('#yt-looper-end').val();
-        }
-        window.open(url);
-      });
+  window.onYouTubePlayerReady = function(playerId) {
+      var newVideo = true;
 
       window.ytPlayerStateChanged = function (state) {
         if (state === 2) {
-          $('#yt-looper-start').val(humanize(ytplayer.getCurrentTime()));
+          $('#yt-looper-start').val(humanize(getYtPlayer().getCurrentTime()));
           $('#yt-looper-interval').fadeIn('slow');
           $button.attr('title','Click to loop selected fragment');
+        } else if (state === 1) {
+          // youtube redraws div.watch-action-buttons AFTER 5 and -1 events
+          // and 1 is the only one we have after GUI stabilizes
+          // ANGST.
+          newVideo = true;
         } else {
           $('#yt-looper-interval').hide();
           $('#yt-looper-start').val(humanize(0));
           $button.attr('title','Click to loop entire video (or pause to select fragment)');
         }
-      };
-
-      ytplayer.addEventListener ('onStateChange', 'ytPlayerStateChanged');
-
-    }
+        if (newVideo) {
+          renderLooperActions();
+          newVideo = false;
+        }
+      }
+      getYtPlayer().addEventListener ('onStateChange', 'ytPlayerStateChanged');
   };
 
 }());
