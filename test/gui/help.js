5188ef2c745297ecc2355c5f94e4082018a25528
Marcin Rataj <lidel@lidel.org>
2015-04-22 21:02:50 +0200

    Increase click timeout in tests

--- test/gui/help.js
+++ test/gui/help.js
@@ -2,18 +2,20 @@ module.exports = {
 
   'Clicking on "help" in #menu' : function (browser) {
     browser.maximizeWindow()
-      .url('http://127.0.0.1:28080/#v=lWqJTKdznaM')
-      .waitForElementVisible('#player', 3000)
-      .assert.hidden('#help')
+      .url('http://127.0.0.1:28080/#i=cJjBEQP.png')
+      .waitForElementVisible('#menu', 30000, false)
       .assert.cssClassNotPresent('#help-toggle', 'ticker')
+      .assert.hidden('#help')
       .click('#help-toggle')
-      .waitForElementVisible('#help', 3000)
-      .assert.visible('#help')
+      .pause(3000)
+      .waitForElementVisible('#help', 30000, false)
       .assert.cssClassPresent('#help-toggle','ticker')
+      .assert.visible('#help')
       .click('#help-toggle')
-      .waitForElementNotVisible('#help', 3000)
-      .assert.hidden('#help')
+      .pause(3000)
+      .waitForElementNotVisible('#help', 30000, false)
       .assert.cssClassNotPresent('#help-toggle', 'ticker')
+      .assert.hidden('#help')
       .end();
   },
 
--- test/gui/random.js
+++ test/gui/random.js
@@ -2,27 +2,27 @@ module.exports = {
 
   'No "random" option for single interval' : function (browser) {
     browser.maximizeWindow()
-      .url('http://127.0.0.1:28080/#v=lWqJTKdznaM')
-      .waitForElementVisible('#player', 3000)
+      .url('http://127.0.0.1:28080/#i=cJjBEQP.png')
+      .waitForElementVisible('#player', 30000)
       .assert.hidden('#random-ui')
       .end();
   },
 
   'Clicking on "random" in multivideo playlist' : function (browser) {
     browser.maximizeWindow()
-      .url('http://127.0.0.1:28080/#v=lWqJTKdznaM&v=ZuHZSbPJhaY')
-      .waitForElementVisible('#player', 3000)
+      .url('http://127.0.0.1:28080/#i=cJjBEQP.png&v=ZuHZSbPJhaY')
+      .waitForElementVisible('#player', 30000, false)
       .assert.visible('#random-ui')
       .assert.cssClassNotPresent('#random', 'ticker')
       .click('#random')
-      .pause(100)
-      .waitForElementVisible('#player', 3000)
-      .assert.urlEquals('http://127.0.0.1:28080/#v=lWqJTKdznaM&v=ZuHZSbPJhaY&random')
+      .pause(3000)
+      .waitForElementVisible('#player', 30000, false)
+      .assert.urlEquals('http://127.0.0.1:28080/#i=cJjBEQP.png&v=ZuHZSbPJhaY&random')
       .assert.cssClassPresent('#random', 'ticker')
       .click('#random')
-      .pause(100)
-      .waitForElementVisible('#player', 3000)
-      .assert.urlEquals('http://127.0.0.1:28080/#v=lWqJTKdznaM&v=ZuHZSbPJhaY')
+      .pause(3000)
+      .waitForElementVisible('#player', 30000, false)
+      .assert.urlEquals('http://127.0.0.1:28080/#i=cJjBEQP.png&v=ZuHZSbPJhaY')
       .assert.cssClassNotPresent('#random', 'ticker')
       .end();
   },
