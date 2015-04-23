6ef76a10de0a0f7c538513995134f407d8c1c38c
Marcin Rataj <lidel@lidel.org>
2015-04-23 13:41:47 +0200

    Even bigger timeouts for superslow TravisCI

--- test/gui/random.js
+++ test/gui/random.js
@@ -3,7 +3,7 @@ module.exports = {
   'No "random" option for single interval' : function (browser) {
     browser.maximizeWindow()
       .url('http://127.0.0.1:28080/#i=cJjBEQP.png')
-      .waitForElementVisible('#player', 30000)
+      .waitForElementVisible('#player', 100000)
       .assert.hidden('#random-ui')
       .end();
   },
@@ -11,17 +11,15 @@ module.exports = {
   'Clicking on "random" in multivideo playlist' : function (browser) {
     browser.maximizeWindow()
       .url('http://127.0.0.1:28080/#i=cJjBEQP.png&v=ZuHZSbPJhaY')
-      .waitForElementVisible('#player', 30000, false)
+      .waitForElementVisible('#player', 100000)
       .assert.visible('#random-ui')
       .assert.cssClassNotPresent('#random', 'ticker')
       .click('#random')
-      .pause(3000)
-      .waitForElementVisible('#player', 30000, false)
+      .waitForElementVisible('#player', 100000)
       .assert.urlEquals('http://127.0.0.1:28080/#i=cJjBEQP.png&v=ZuHZSbPJhaY&random')
       .assert.cssClassPresent('#random', 'ticker')
       .click('#random')
-      .pause(3000)
-      .waitForElementVisible('#player', 30000, false)
+      .waitForElementVisible('#player', 100000)
       .assert.urlEquals('http://127.0.0.1:28080/#i=cJjBEQP.png&v=ZuHZSbPJhaY')
       .assert.cssClassNotPresent('#random', 'ticker')
       .end();
