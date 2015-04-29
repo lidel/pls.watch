0d3a52d38a697807ea5d3286940860aba3fae9e1
Marcin Rataj <lidel@lidel.org>
2015-04-29 22:23:34 +0200

    Always run jshint

--- Gruntfile.js
+++ Gruntfile.js
@@ -121,13 +121,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', 'travis');
+  grunt.registerTask('default', 'test');
+  grunt.registerTask('common',  ['env', 'jshint', 'qunit', 'env:test', 'connect', 'selenium']);
 
-  grunt.registerTask('test',    ['env', 'qunit', 'env:test', 'connect', 'selenium', 'nightwatch:phantomjs']);
-  grunt.registerTask('firefox', ['env', 'qunit', 'env:test', 'connect', 'selenium', 'nightwatch:firefox']);
-  grunt.registerTask('chrome',  ['env', 'qunit', 'env:test', 'connect', 'selenium', 'nightwatch:chrome']);
+  grunt.registerTask('test',    ['common', 'nightwatch:phantomjs']);
+  grunt.registerTask('firefox', ['common', 'nightwatch:firefox']);
+  grunt.registerTask('chrome',  ['common', 'nightwatch:chrome']);
 
-  grunt.registerTask('travis',  ['jshint', 'test']);
 
   grunt.registerTask('httpd', 'connect:server:keepalive');
 
