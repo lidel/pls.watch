e5782af2100063ef7629c2f4c0181590f5d539ac
Marcin Rataj <lidel@lidel.org>
2016-07-03 20:05:12 +0200

    jquery.mcustomscrollbar 3.1.4

--- yt-looper.js
+++ yt-looper.js
@@ -670,8 +670,8 @@ function showHelpUi(show) {
             $('<link>')
               .appendTo('head')
               .attr({type : 'text/css', rel : 'stylesheet'})
-              .attr('href', 'https://cdn.jsdelivr.net/jquery.mcustomscrollbar/3.1.1/jquery.mCustomScrollbar.min.css');
-            return 'https://cdn.jsdelivr.net/jquery.mcustomscrollbar/3.1.1/jquery.mCustomScrollbar.concat.min.js';
+              .attr('href', 'https://cdn.jsdelivr.net/jquery.mcustomscrollbar/3.1.4/jquery.mCustomScrollbar.min.css');
+            return 'https://cdn.jsdelivr.net/jquery.mcustomscrollbar/3.1.4/jquery.mCustomScrollbar.concat.min.js';
           } else {
             return null;
           }
