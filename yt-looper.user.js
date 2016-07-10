b2b013e6ef506acb831646a06a86b36a65d62b73
Marcin Rataj <lidel@lidel.org>
2016-07-10 19:03:31 +0200

    Userscript updates for YouTube
    - polls DOM -- works with reactive UI
    - jquery update
    - does not run in iframe contexts
    - closes #195

--- yt-looper.user.js
+++ yt-looper.user.js
@@ -3,70 +3,71 @@
 // @description Adds a button on YouTube, Imgur and SoundCloud to open current resource in yt.aergia.eu looper
 // @namespace   https://yt.aergia.eu
 // @icon        https://i.imgur.com/EGgL1nx.png
-// @include     http://www.youtube.com/watch*
-// @include     http://youtube.com/watch*
-// @include     https://www.youtube.com/watch*
-// @include     https://youtube.com/watch*
+// @include     https://www.youtube.com/*
+// @include     https://youtube.com/*
+// @include     http://www.youtube.com/*
+// @include     http://youtube.com/*
 // @include     https://imgur.com/*
 // @include     http://imgur.com/*
 // @include     http://soundcloud.com/*
 // @include     https://soundcloud.com/*
-// @version     1.6.4
+// @version     1.6.5
 // @updateURL   https://yt.aergia.eu/yt-looper.user.js
 // @downloadURL https://yt.aergia.eu/yt-looper.user.js
-// @require     https://cdn.jsdelivr.net/jquery/3.0.0-beta1/jquery.min.js
+// @require     https://cdn.jsdelivr.net/jquery/3.1.0/jquery.min.js
 // @grant       none
+// @noframes
 // ==/UserScript==
 (function ($, undefined) { // eslint-disable-line no-unused-vars
   'use strict';
   var youtubeHandler = function () {
-    var humanize = function (sec) {
-      var sec_num = parseInt(sec, 10);
-      var hours = Math.floor(sec_num / 3600);
-      var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
-      var seconds = sec_num - (hours * 3600) - (minutes * 60);
-      hours = hours > 0 ? (hours + 'h')  : '';
-      minutes = minutes > 0 ? (minutes + 'm')  : '';
-      seconds = seconds > 0 ? (seconds + 's')  : '';
-      return sec === 0 ? '0s' : hours + minutes + seconds;
-    };
-    var getYtPlayer = function () {
-      var doc = window.document;
-      var ytplayer = doc.getElementById('movie_player') || doc.getElementById('movie_player-flash');
-      return ytplayer;
-    };
-    var renderLooperActions = function() {
-      $('#yt-looper-start').remove();
-      $('#yt-looper-end').remove();
-      $('#yt-looper').remove();
-      $('#yt-looper-interval').remove();
-      var $button = $('<button id="yt-looper" class="yt-uix-button yt-uix-button-opacity yt-uix-button-has-icon yt-uix-tooltip" title="Open in yt-looper"><span style="margin:0 0.1em;font-size:2.25em;vertical-align: middle;">&#x21BB;</span> yt.aergia.eu </button>').hide();
-      var $intervals = $('<span id="yt-looper-interval"></span>').hide();
-      var input = '<input class="yt-uix-form-input-text title-input" style="width:50px!important;margin-right:0.25em;" />';
-      var $secondaryActions = $('div.watch-action-buttons > div.watch-secondary-actions');
-      var start = humanize(0);
-      var end = humanize(getYtPlayer().getDuration());
-      var $start = $(input).attr('id', 'yt-looper-start').val(start).attr('title', 'Start of loop');
-      var $end = $(input).attr('id', 'yt-looper-end').val(end).attr('title', 'End of loop');
-      $secondaryActions.prepend($button);
-      $intervals.append($start).append($end).insertBefore($button);
-      $button.show();
-      $button.click(function () {
-        var url = window.location.href;
-        url = url.replace(/.*youtube.com\/watch/, 'https://yt.aergia.eu/');
-        if (getYtPlayer().getPlayerState() === 2) {
-          url = url.replace(/[&#]t=[^&#]*/g, '');
-          url = url + '#t=' + $('#yt-looper-start').val() + ';' + $('#yt-looper-end').val();
-        }
-        window.open(url);
-      });
-    };
+    var atWatchPage = window.location.pathname.startsWith('/watch');
+    if (atWatchPage && $('#yt-looper').length === 0) {
+      var humanize = function (sec) {
+        var sec_num = parseInt(sec, 10);
+        var hours = Math.floor(sec_num / 3600);
+        var minutes = Math.floor((sec_num - (hours * 3600)) / 60);
+        var seconds = sec_num - (hours * 3600) - (minutes * 60);
+        hours = hours > 0 ? (hours + 'h')  : '';
+        minutes = minutes > 0 ? (minutes + 'm')  : '';
+        seconds = seconds > 0 ? (seconds + 's')  : '';
+        return sec === 0 ? '0s' : hours + minutes + seconds;
+      };
+      var getYtPlayer = function () {
+        var doc = window.document;
+        var ytplayer = doc.getElementById('movie_player') || doc.getElementById('movie_player-flash');
+        return ytplayer;
+      };
+      var renderLooperActions = function() {
+        $('#yt-looper-start').remove();
+        $('#yt-looper-end').remove();
+        $('#yt-looper').remove();
+        $('#yt-looper-interval').remove();
+        var $button = $('<button id="yt-looper" class="yt-uix-button yt-uix-button-opacity yt-uix-button-has-icon yt-uix-tooltip" title="Open in yt-looper"><span style="margin:0 0.1em;font-size:2.25em;vertical-align: middle;">&#x21BB;</span> yt.aergia.eu </button>').hide();
+        var $intervals = $('<span id="yt-looper-interval"></span>').hide();
+        var input = '<input class="yt-uix-form-input-text title-input" style="width:50px!important;margin-right:0.25em;" />';
+        var $secondaryActions = $('div.watch-action-buttons > div.watch-secondary-actions');
+        var start = humanize(0);
+        var end = humanize(getYtPlayer().getDuration());
+        var $start = $(input).attr('id', 'yt-looper-start').val(start).attr('title', 'Start of loop');
+        var $end = $(input).attr('id', 'yt-looper-end').val(end).attr('title', 'End of loop');
+        $secondaryActions.prepend($button);
+        $intervals.append($start).append($end).insertBefore($button);
+        $button.show();
+        $button.click(function () {
+          var url = window.location.href;
+          url = url.replace(/.*youtube.com\/watch/, 'https://yt.aergia.eu/');
+          if (getYtPlayer().getPlayerState() === 2) {
+            url = url.replace(/[&#]t=[^&#]*/g, '');
+            url = url + '#t=' + $('#yt-looper-start').val() + ';' + $('#yt-looper-end').val();
+          }
+          window.open(url);
+        });
+      };
 
-    var initButton = function() {
-      window.onYouTubePlayerReady = function () {
-        console.log('yt-looper @ onYouTubePlayerReady');
+      var initButton = function() {
         var newVideo = true;
-        window.ytPlayerStateChanged = function (state) {
+        window.ytLooperStateChanged = function (state) {
           if (state === 2) {
             $('#yt-looper-start').val(humanize(getYtPlayer().getCurrentTime()));
             $('#yt-looper-interval').fadeIn('slow');
@@ -85,10 +86,11 @@
           }
         };
         renderLooperActions();
-        getYtPlayer().addEventListener('onStateChange', 'ytPlayerStateChanged');
+        getYtPlayer().addEventListener('onStateChange', 'ytLooperStateChanged');
       };
-    };
-    initButton();
+
+      initButton();
+    }
   };
   var imgurHandler = function () {
     var $oldButton = $('li#yt-looper');
@@ -132,15 +134,18 @@
       }
     }
   };
+  // since everyone does reactive gui,
+  // just poll DOM continuously and react when elements appear/disappear
+  console.log('yt-looper userscript loaded');
   switch (window.location.hostname.replace('www.', '')) {
     case 'youtube.com':
-      youtubeHandler();
+      window.setInterval(youtubeHandler, 2000);
       break;
     case 'imgur.com':
-      window.setInterval(imgurHandler, 1000); // quick hack for reactive gui
+      window.setInterval(imgurHandler, 1000);
       break;
     case 'soundcloud.com':
-      window.setInterval(soundCloudHandler, 1000); // same
+      window.setInterval(soundCloudHandler, 1000);
       break;
   }
 }(window.jQuery.noConflict(true)));
