ff7060afc30453140ccd798c9d88c2ad617d82fd
Marcin Rataj <lidel@lidel.org>
2020-10-16 01:02:13 +0200

    fix: content-type on IPFS gateways

--- deploy.sh
+++ deploy.sh
@@ -8,8 +8,8 @@ cp -r favicon.ico pls.*.js index.html style.css assets/ dist/
 
 sed -i  \
     -e "s/%git-hash%/$GIT_HEAD/" \
-    -e "s/css\"/css?${GIT_HEAD}\"/" \
-    -e "s/'pls.watch.js'/'pls.watch.js?${GIT_HEAD}'/" \
-    -e "s/'pls.watch.editor.js'/'pls.watch.editor.js?${GIT_HEAD}'/" \
+    -e "s/css\"/css?${GIT_HEAD}.css\"/" \
+    -e "s/'pls.watch.js'/'pls.watch.js?${GIT_HEAD}.js'/" \
+    -e "s/'pls.watch.editor.js'/'pls.watch.editor.js?${GIT_HEAD}.js'/" \
     dist/index.html
 
